4fa49ebff408bd75a8823cdcd71ecb8d38f96258
ahmed mahmoud <ahmedmahmous876234@gmail.com>
2026-05-18 21:32:39 +0300

    create icons and images file and create core folder

---
+++ lib/core/utils/icons.dart
@@ -0,0 +1,10 @@
+class AppIcons {
+  static const String addTask = 'assets/icons/add.svg';
+  static const String search = 'assets\icons\delete.svg';
+  static const String add = 'assets\icons\log_out_icon.svg';
+  static const String notification = 'assets\icons\perority_icon.svg';
+  static const String profile = 'assets\icons\send.svg';
+  static const String profile = 'assets\icons\tassky_icon.svg';
+
+  static const String profile = 'assets\icons\time_icon.svg';
+}
